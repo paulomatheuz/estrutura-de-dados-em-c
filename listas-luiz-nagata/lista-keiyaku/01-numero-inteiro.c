06323006efc4625d995b1504166d6cd00a26293d
paulomatheuz <paulo.okkotsu@gmail.com>
2026-09-07 20:19:19 -0400

    feat: adiciona questão 1 da lista Keiyaku

---
+++ listas-luiz-nagata/lista-keiyaku/01-numero-inteiro.c
@@ -0,0 +1,16 @@
+/* Enunciado:
+ * Faça um programa que leia um número inteiro e o imprima.
+ */
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    printf("O numero digitado foi: %d\n", numero);
+
+    return 0;
+}
